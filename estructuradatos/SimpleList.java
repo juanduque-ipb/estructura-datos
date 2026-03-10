223ed5391230b18edad549a40d085cc7baa8d151
juanduque-ipb <juan.duque@pascualbravo.edu.co>
2026-03-09 19:39:09 -0500

    Update SimpleList.java

--- estructuradatos/SimpleList.java
+++ estructuradatos/SimpleList.java
@@ -41,7 +41,59 @@ public class SimpleList {
         System.out.print("null");
     }
     
+    public boolean Search(int dato){
+        if(Head == null) {
+            System.out.println("La lista esta vacia");
+            return false;
+        }
+        
+        if(Head.Dato == dato) {
+            System.out.println("Dato encontrado, en HEAD");
+            return true;
+        }
+        
+        Nodo Actual = Head;
+        boolean flag = false;
+        while(Actual != null){
+            if(Actual.Dato == dato) {
+                System.out.println("Dato Encontrado en:"+Actual.toString());
+                flag = true;
+                break;
+            }
+            Actual = Actual.Puntero;
+        }
+        
+        return flag;
+    }
     
+    public void Remove(int dato) {
+         if(Head == null) {
+            System.out.println("La lista esta vacia");
+            return;
+        }
+        
+        if(Head.Dato == dato) {
+            System.out.println("Dato removido, en HEAD");
+            Head = null;
+            return;
+        }
+        
+        Nodo Actual = Head;
+        while(Actual.Puntero != null && Actual.Puntero.Dato != dato){
+            Actual = Actual.Puntero;
+        }
+        
+        if(Actual == null) {
+            System.out.println("Dato no encontrado");
+        } else {
+            System.out.println("MEMORIA:"+Actual.Puntero.toString());
+            System.out.println("MEMORIA:"+Actual.Puntero.Puntero.toString());
+
+            Actual.Puntero = Actual.Puntero.Puntero;
+            System.out.println("Dato Eliminado");
+        }
+    }
     
     
 }
+
